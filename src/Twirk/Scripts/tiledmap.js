48016b68c379f5de28d556afb85249c1545d76d3
Brian Holley <brian.holley@hotmail.com>
2015-09-12 21:03:24 -0700

    Switch computation from map image to tile map

--- src/Twirk/Scripts/tiledmap.js
+++ src/Twirk/Scripts/tiledmap.js
@@ -65,22 +65,31 @@ function renderTiledMap(mapDiv) {
 	for (var y = 0; y < board_height; y++) {
 		for (var x = 0; x < board_width; x++) {
 			var tileIndex = board_backgrounds[x + y * board_width];
-			var tileX = (tileIndex % tileset_width) * tile_size;
-			var tileY = Math.floor(tileIndex / tileset_width) * tile_size;
-			html += "<div style=\"width:" + tile_size + "px;height:" + tile_size + "px;background:url('images/roborally-standard-50dpi-flat-v2.png') -" + tileX + "px -" + tileY + "px;float:left;\">";
+			var tileX = -(tileIndex % tileset_width) * tile_size;
+			var tileY = -Math.floor(tileIndex / tileset_width) * tile_size;
+
+			var backgroundDiv = document.createElement("div");
+			backgroundDiv.style.width = tile_size + "px";
+			backgroundDiv.style.height = tile_size + "px";
+			backgroundDiv.style.background = "url('images/roborally-standard-50dpi-flat-v2.png') " + tileX + "px " + tileY + "px";
+			backgroundDiv.style.float = "left";
 
 			var doodadIndex = board_doodads[x + y * board_width];
 			if (doodadIndex != -1) {
-				var doodadX = (doodadIndex % tileset_width) * tile_size;
-				var doodadY = Math.floor(doodadIndex / tileset_width) * tile_size;
-				html += "<div style=\"width:" + tile_size + "px;height:" + tile_size + "px;background:url('images/roborally-standard-50dpi-flat-v2.png') -" + doodadX + "px -" + doodadY + "px;float:left;\">&nbsp</div>";
+				var doodadX = -(doodadIndex % tileset_width) * tile_size;
+				var doodadY = -Math.floor(doodadIndex / tileset_width) * tile_size;
+
+				var doodadDiv = document.createElement("div");
+				doodadDiv.style.width = tile_size + "px";
+				doodadDiv.style.height = tile_size + "px";
+				doodadDiv.style.background = "url('images/roborally-standard-50dpi-flat-v2.png') " + doodadX + "px " + doodadY + "px";
+				backgroundDiv.appendChild(doodadDiv);
 			}
-			else {
-				html += "&nbsp;";
-			}
-			html += "</div>";
+			mapDiv.appendChild(backgroundDiv);
 		}
 	}
-	html += "<div style=\"clear:left;\"/>";
-	mapDiv.innerHTML = html;
+
+	var clearDiv = document.createElement("div");
+	clearDiv.style.clear = "left";
+	mapDiv.appendChild(clearDiv);
 }
